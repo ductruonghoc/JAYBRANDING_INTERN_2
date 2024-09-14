99f9f8ca0438eb98953bc90fc53de63253b21544
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-09-14 16:09:39 +0700

    1.0.1
    Header: Desktop
    Header: Mobile

--- jaybranding_intern/src/app/header.tsx
+++ jaybranding_intern/src/app/header.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image"
 import Navigator from "./navigator";
 import SocialMediaComponent from "./social_media";
+import { Bars3BottomLeftIcon } from "@heroicons/react/24/outline";
 
 const HKKLogoSrc:string = "https://hkkzipper.com/wp-content/uploads/2022/07/HKK-trans-e1678259002605.png.webp"; //Logo src
 
 export default function Header(){
     return (
         <div
-            className="w-full py-[4px]
-                        flex gap-[74px] justify-center items-center
-                        bg-white"
+            className="w-full md:py-[4px]
+                        flex md:gap-[74px] md:justify-center items-center
+                        bg-white
+                        max-md:py-[9px] max-md:px-[3px] max-md:justify-between max-md:flex-row-reverse"
         >
             <div>{/*Logo */}
                 <Image
@@ -21,9 +23,14 @@ export default function Header(){
             </div>{/*NAv bar */}
             <div
                 className="
-                            flex flex-col gap-[9px] items-end justify-center">
+                            flex flex-col gap-[9px] items-end justify-center max-md:hidden">
                 <SocialMediaComponent/>
                 <Navigator/>
+            </div>
+            <div className="md:hidden">
+                <Bars3BottomLeftIcon
+                    className="cursor-pointer"
+                    height={40}/>
             </div>
         </div>
     )
--- jaybranding_intern/src/app/social_media.tsx
+++ jaybranding_intern/src/app/social_media.tsx
@@ -1,3 +1,33 @@
+import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/outline"
+
+const UKFlag = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 32 32">
+            <rect x="1" y="4" width="30" height="24" fill="#071b65"></rect>
+            <path d="M5.101,4h-.101c-1.981,0-3.615,1.444-3.933,3.334L26.899,28h.101c1.981,0,3.615-1.444,3.933-3.334L5.101,4Z" fill="#fff"></path><path d="M22.25,19h-2.5l9.934,7.947c.387-.353,.704-.777,.929-1.257l-8.363-6.691Z" fill="#b92932"></path><path d="M1.387,6.309l8.363,6.691h2.5L2.316,5.053c-.387,.353-.704,.777-.929,1.257Z" fill="#b92932"></path><path d="M5,28h.101L30.933,7.334c-.318-1.891-1.952-3.334-3.933-3.334h-.101L1.067,24.666c.318,1.891,1.952,3.334,3.933,3.334Z" fill="#fff"></path><rect x="13" y="4" width="6" height="24" fill="#fff"></rect><rect x="1" y="13" width="30" height="6" fill="#fff"></rect><rect x="14" y="4" width="4" height="24" fill="#b92932"></rect><rect x="14" y="1" width="4" height="30" transform="translate(32) rotate(90)" fill="#b92932"></rect><path d="M28.222,4.21l-9.222,7.376v1.414h.75l9.943-7.94c-.419-.384-.918-.671-1.471-.85Z" fill="#b92932"></path><path d="M2.328,26.957c.414,.374,.904,.656,1.447,.832l9.225-7.38v-1.408h-.75L2.328,26.957Z" fill="#b92932"></path><path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path><path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path>
+        </svg>
+    )
+}
+
+const VNFlag = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 32 32">
+            <rect x="1" y="4" width="30" height="24" fill="#c93728">
+            </rect>
+            <path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path><path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path><path fill="#ff5" d="M18.008 16.366L21.257 14.006 17.241 14.006 16 10.186 14.759 14.006 10.743 14.006 13.992 16.366 12.751 20.186 16 17.825 19.249 20.186 18.008 16.366z">
+            </path>
+        </svg>
+    )
+}
+
 const FacebookLogo = () => {
     return (
         <svg
@@ -24,6 +54,19 @@ const YoutubeLogo = () => {
     )
 }
 
+const ZaloLogo = () => {
+    return (
+        <strong
+            className="w-[20px] h-[20px]
+                        flex justify-center items-center
+                        bg-gray-400
+                        rounded-full
+                        text-[white] text-[7px]">
+            Zalo
+        </strong>
+    )
+}
+
 interface SocialMedia {
     svg: React.ReactNode,
     name: string
@@ -37,30 +80,59 @@ const socialMediaList: SocialMediaList = [
         name: "Facebook"
     },
     {
-        svg: <YoutubeLogo/>,
+        svg: <YoutubeLogo />,
         name: "Youtube"
+    },
+    {
+        svg: <ZaloLogo />,
+        name: "Zalo"
     }
 ]
 
 export default function SocialMediaComponent() {
     return (
         <div
-            className="py-[5px]
-                        flex gap-[16px]
+            className="flex items-center gap-[16px]
+                        text-nowrap">
+            <div
+                className="py-[5px]
+                        flex gap-[8px]
                         text-gray-400">
-            {
-                socialMediaList.map((_, idx) => (
-                    <div
-                        className="flex items-center gap-[7px]
+                {
+                    socialMediaList.map((_, idx) => (
+                        <div
+                            className="flex items-center gap-[7px]
                                     cursor-pointer"
-                        key={idx}>
-                        {_.svg}
-                        <div>
-                            {_.name}
+                            key={idx}>
+                            {_.svg}
+                            <div>
+                                {_.name}
+                            </div>
                         </div>
+                    ))
+                }
+            </div>
+            <div
+                className="h-[19px]
+                            border-x border-gray-400"/>
+            <div
+                className="flex gap-[4px] items-center
+                            cursor-pointer">
+                <div className="flex items-center">
+                    <ArrowLeftEndOnRectangleIcon
+                        height={18}
+                        className="stroke-gray-400" />
+                    <div
+                        className="text-[13px]">
+                        ĐĂNG NHẬP
                     </div>
-                ))
-            }
+                </div>
+                <div
+                    className="flex gap-[6px]">
+                    <VNFlag></VNFlag>
+                    <UKFlag/>
+                </div>
+            </div>
         </div>
     )
 }
