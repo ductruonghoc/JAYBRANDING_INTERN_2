04370877628aa80ef1406182aa4c36aa81ce1df9
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-09-16 13:36:31 +0700

    1.0.4
    introduce::desktop
    introduce::mobile

--- jaybranding_intern/src/app/Intro.tsx
+++ jaybranding_intern/src/app/Intro.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Branding from "./branding";
 import Background from "./img/white_geometric_bg.webp";
 import Image from "next/image";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 interface AttractFact {
     no: number,
@@ -27,14 +28,14 @@ const attractFacts: AttractFacts = [
 
 const YoutubeLogo = () => {
     return (
-        <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            x="0px" y="0px" 
-            width="18" 
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px" y="0px"
+            width="18"
             height="18"
             className="fill-sky-700"
             viewBox="0 0 50 50">
-                <path d="M 24.402344 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.402344 16.898438 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.902344 40.5 17.898438 41 24.5 41 C 31.101563 41 37.097656 40.5 40.597656 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.097656 35.5 C 45.5 33 46 29.402344 46.097656 24.902344 C 46.097656 20.402344 45.597656 16.800781 45.097656 14.300781 C 44.699219 12.101563 42.800781 10.5 40.597656 10 C 37.097656 9.5 31 9 24.402344 9 Z M 24.402344 11 C 31.601563 11 37.398438 11.597656 40.199219 12.097656 C 41.699219 12.5 42.898438 13.5 43.097656 14.800781 C 43.699219 18 44.097656 21.402344 44.097656 24.902344 C 44 29.199219 43.5 32.699219 43.097656 35.199219 C 42.800781 37.097656 40.800781 37.699219 40.199219 37.902344 C 36.597656 38.601563 30.597656 39.097656 24.597656 39.097656 C 18.597656 39.097656 12.5 38.699219 9 37.902344 C 7.5 37.5 6.300781 36.5 6.101563 35.199219 C 5.300781 32.398438 5 28.699219 5 25 C 5 20.398438 5.402344 17 5.800781 14.902344 C 6.101563 13 8.199219 12.398438 8.699219 12.199219 C 12 11.5 18.101563 11 24.402344 11 Z M 19 17 L 19 33 L 33 25 Z M 21 20.402344 L 29 25 L 21 29.597656 Z"></path>
+            <path d="M 24.402344 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.402344 16.898438 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.902344 40.5 17.898438 41 24.5 41 C 31.101563 41 37.097656 40.5 40.597656 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.097656 35.5 C 45.5 33 46 29.402344 46.097656 24.902344 C 46.097656 20.402344 45.597656 16.800781 45.097656 14.300781 C 44.699219 12.101563 42.800781 10.5 40.597656 10 C 37.097656 9.5 31 9 24.402344 9 Z M 24.402344 11 C 31.601563 11 37.398438 11.597656 40.199219 12.097656 C 41.699219 12.5 42.898438 13.5 43.097656 14.800781 C 43.699219 18 44.097656 21.402344 44.097656 24.902344 C 44 29.199219 43.5 32.699219 43.097656 35.199219 C 42.800781 37.097656 40.800781 37.699219 40.199219 37.902344 C 36.597656 38.601563 30.597656 39.097656 24.597656 39.097656 C 18.597656 39.097656 12.5 38.699219 9 37.902344 C 7.5 37.5 6.300781 36.5 6.101563 35.199219 C 5.300781 32.398438 5 28.699219 5 25 C 5 20.398438 5.402344 17 5.800781 14.902344 C 6.101563 13 8.199219 12.398438 8.699219 12.199219 C 12 11.5 18.101563 11 24.402344 11 Z M 19 17 L 19 33 L 33 25 Z M 21 20.402344 L 29 25 L 21 29.597656 Z"></path>
         </svg>
     )
 }
@@ -50,6 +51,12 @@ const buttonContents: ButtonContents = [
     {
         svg: <YoutubeLogo />,
         text: "VIDEO GIỚI THIỆU"
+    },
+    {
+        svg: <ArrowRightIcon
+            className="stroke-sky-700"
+            width={12} />,
+        text: "TÌM HIỂU THÊM"
     }
 ]
 
@@ -73,32 +80,40 @@ const Information = () => {
             <div
                 className="relative">
                 <div
-                    className="absolute top:0
-                                flex flex-col items-center">
+                    className="absolute top-0
+                                flex flex-col items-center
+                                max-md:left-[9px]">
                     <div
                         className="h-[86px]
-                                    border-x border-gray-200"></div>
+                                    border-x border-gray-200
+                                    max-md:hidden"></div>
                     <strong
                         className="text-nowrap text-[72px] leading-none text-white text-stroke">
                         01
                     </strong>
                 </div>
                 <div
-                    className="pt-[103px]">
+                    className="md:pt-[103px]
+                                max-md:pt-[16px]">
                     <div
-                        className="flex flex-col gap-[28px]
-                        ">
+                        className="flex flex-col md:gap-[28px]
+                                    max-md:gap-[26px]">
                         <strong
                             className="ps-[85px]
-                                        text-[40px] text-sky-700 text-nowrap">
+                                        text-[40px] text-sky-700 text-nowrap text-center
+                                        max-md:ps-0">
                             GIỚI THIỆU
                         </strong>
                         <strong
-                            className="text-[24px] text-nowrap">
+                            className="text-[24px] text-nowrap 
+                                        max-md:text-center">
                             VỀ KÉO HOÀN MỸ - HKK
                         </strong>
                         <div
-                            className="w-[457px]">
+                            className="md:w-[457px]
+                                        max-md:text-center
+                                        max-md:w-full
+                                        max-md:px-[10px]">
                             {
                                 "Hoàn thành thành lập năm 1989, là một trong những công ty đi đầu tiên trong lĩnh vực sản xuất dây khóa tại Việt Nam. Với công nghệ, hiện đại thiết bị và đồng bộ của Đức, Đài Loan cùng với đội ngũ kỹ sư, nhân viên có trình độ nghiệp vụ cao, sản phẩm dây kéo HKK được sản xuất theo tiêu chuẩn quốc tế (ASTM D2016 : 2007, BS 3084: 2006, CNS, JIS-S 3015 ....)."
                             }
@@ -109,7 +124,8 @@ const Information = () => {
             <div
                 className="flex flex-col gap-[19px]">
                 <div
-                    className="flex flex-col gap-[15px]">
+                    className="flex flex-col gap-[15px]
+                                max-md:px-[9px]">
                     {
                         attractFacts.map((_, idx) => {
                             return (
@@ -140,23 +156,27 @@ const Information = () => {
                     }
                 </div>
                 <div
-                    className="flex gap-[20px]">
-                    {
-                        buttonContents.map((_, idx) => {
-                            return (
-                                <button
-                                    key={idx}
-                                    className="button">
-                                    <div>
-                                        {_.svg}
-                                    </div>
-                                    <div>
-                                        {_.text}
-                                    </div>
-                                </button>
-                            )
-                        })
-                    }
+                    className="flex max-md:justify-center">
+                    <div
+                        className="flex gap-[20px]
+                                    max-md:flex-col">
+                        {
+                            buttonContents.map((_, idx) => {
+                                return (
+                                    <button
+                                        key={idx}
+                                        className="button">
+                                        <div>
+                                            {_.svg}
+                                        </div>
+                                        <div>
+                                            {_.text}
+                                        </div>
+                                    </button>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
@@ -166,7 +186,7 @@ const Information = () => {
 export default function Introduce() {
     return (
         <div
-            className="relative w-full pb-[90px]
+            className="relative w-full
                     flex justify-center">
             <div
                 className="absolute top-0
@@ -183,7 +203,10 @@ export default function Introduce() {
                 </Image>
             </div>
             <div
-                className="relative flex gap-[90px]">
+                className="relative md:pb-[90px]
+                            flex md:gap-[90px]
+                            max-md:flex-col
+                            max-md:pb-[36px] max-md:gap-[36px]">
                 <Branding></Branding>
                 <Information />
             </div>
--- jaybranding_intern/src/app/branding.tsx
+++ jaybranding_intern/src/app/branding.tsx
@@ -4,63 +4,73 @@ export default function Branding() {
     return (
         <div
             className="relative
-                     pt-[90px]
-                     flex items-center justify-center">
+                     md:pt-[90px]
+                     flex items-center justify-center
+                     max-md:pt-[36px]">
             <div
                 className="
                     relative z-0
-                    w-[560px] h-[639px]
-                    flex gap-[6px]"
+                    md:w-[560px] md:h-[639px]
+                    flex md:gap-[6px]
+                    max-md:w-[391px] max-md:h-[446px] max-md:gap-[9px]"
             >
                 <div
-                    className="w-[274px]
-                                flex flex-col gap-[6px]">
+                    className="md:w-[274px]
+                                flex flex-col md:gap-[6px]
+                                max-md:w-[191px] max-md:gap-[9px]">
                     <div
                         className="
                             w-full                          
-                            h-[304px]
+                            md:h-[304px]
                             rounded-[12px]
-                            bg-[url('img/bluebag.jpg')] bg-[length:560px_639px] bg-left-top">
+                            bg-[url('img/bluebag.jpg')] md:bg-[length:560px_639px] bg-left-top
+                            max-md:h-[212px] max-md:bg-[length:391px_446px]">
                     </div>
                     <div
                         className="
                             w-full                          
                             h-[323px]
                             rounded-[12px]
-                            bg-[url('img/bluebag.jpg')] bg-[length:560px_639px] bg-left-bottom">
+                            bg-[url('img/bluebag.jpg')] md:bg-[length:560px_639px] bg-left-bottom
+                            max-md:h-[226px] max-md:bg-[length:391px_446px]">
 
                     </div>
                 </div>
                 <div
-                    className="w-[274px]
-                                flex flex-col gap-[6px]">
+                    className="md:w-[274px]
+                                flex flex-col md:gap-[6px]
+                                max-md:w-[191px] max-md:gap-[9px]">
                     <div
                         className="
                             w-full                          
                             h-[355px]
                             rounded-[12px]
-                            bg-[url('img/bluebag.jpg')] bg-[length:560px_639px] bg-right-top">
+                            bg-[url('img/bluebag.jpg')] md:bg-[length:560px_639px] bg-right-top
+                            max-md:h-[248px] max-md:bg-[length:391px_446px]">
                     </div>
                     <div
                         className="
                             w-full                          
-                            h-[238px]
+                            md:h-[238px]
                             rounded-[12px]
-                            bg-[url('img/bluebag.jpg')] bg-[length:560px_639px] bg-[right_bottom_-36px]">
+                            bg-[url('img/bluebag.jpg')] bg-[length:560px_639px] bg-[right_bottom_-36px]
+                            max-md:h-[148px] max-md:bg-[length:391px_446px] max-md:bg-[right_bottom_-41px]">
 
                     </div>
                 </div>
             </div>
             <div
                 className="absolute 
-                            w-[202px] h-[202px]
+                            md:w-[202px] md:h-[202px]
                             rounded-full
                             bg-white
                             flex justify-center items-center
-                            outline outline-1 outline-offset-[27px] outline-white">
+                            outline outline-1 md:outline-offset-[27px] outline-white
+                            max-md:w-[160px] max-md:h-[160px] max-md:outline-offset-[21px]">
                 <Image
                     src={HKKLogoSrc}
                     alt="HKK Logo"
+                    className="max-md:h-[39px] max-md:w-[109px]"
                     width={138}
                     height={49}/>
             </div>
